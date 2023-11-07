15e5cf55ab1e75de0fd3758c878f0f269d056412
ArtSnow <vladike26@gmail.com>
2023-11-07 15:59:37 +0000

    adsas

--- travelservice/user_server/database/database.cpp
+++ travelservice/user_server/database/database.cpp
@@ -1,36 +1,3 @@
-// #include "database.h"
-// #include "../config/config.h"
-
-// namespace database{
-//     Database::Database(){
-//         _connection_string+="host=";
-//         _connection_string+=Config::get().get_host();
-//         _connection_string+=";user=";
-//         _connection_string+=Config::get().get_login();
-//         _connection_string+=";db=";
-//         _connection_string+=Config::get().get_database();
-//         _connection_string+=";port=";
-//         _connection_string+=Config::get().get_port();
-//         _connection_string+=";password=";
-//         _connection_string+=Config::get().get_password();
-
-//         std::cout << "Connection string:" << _connection_string << std::endl;
-//          Poco::Data::MySQL::Connector::registerConnector();
-//         _pool = std::make_unique<Poco::Data::SessionPool>(Poco::Data::MySQL::Connector::KEY, _connection_string);
-//     }
-
-//     Database& Database::get(){
-//         static Database _instance;
-//         return _instance;
-//     }
-
-//     Poco::Data::Session Database::create_session(){
-//         return Poco::Data::Session(_pool->get());
-//     }
-
-// }
-
-
 #include "database.h"
 #include "../config/config.h"
 #include <functional>
@@ -64,23 +31,6 @@ namespace database{
         }
         return result;
     }
-
-    // for messenger upd
-    std::string Database::sharding_hint_old(long from, long to){
-
-        std::string key;
-
-        key += std::to_string(from);
-        key += ";";
-        key += std::to_string(to);
-
-        size_t shard_number = std::hash<std::string>{}(key)%get_max_shard();
-
-        std::string result = "-- sharding:";
-        result += std::to_string(shard_number);
-        return result;
-    }
-
 
     std::string Database::sharding_hint(long id){
 
