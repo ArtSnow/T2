147b4dbb0b294ce79342a73a7b71213ea3da7aef
ArtSnow <vladike26@gmail.com>
2023-11-07 15:45:42 +0000

    1

--- travelservice/start_route.sh
+++ travelservice/start_route.sh
@@ -3,8 +3,8 @@ export DB_PORT=3360
 export DB_LOGIN=traveluser
 export DB_PASSWORD=traveluser
 export DB_DATABASE=traveldb
-export CACHE=127.0.0.1:6379
+
 
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
-./tmpbuild/load_data
+./build/route_server
